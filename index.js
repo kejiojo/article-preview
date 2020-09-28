b712b6367e200fe73563e159d23fbbf4d1eca768
Keji-so <ojokeji@gmail.com>
2020-09-28 15:12:49 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+function onClick() {
+    $('.show').fadeToggle();
+   var popUp = document.querySelector('#popup');
+   popUp.classList.toggle('show');
+}
+
+document.querySelector('.share-div').addEventListener('click', onClick);
